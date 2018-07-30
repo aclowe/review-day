a5b22ee595cec0b039bfc2f0bd6eaba20439c63c
Adnan Yousef <yousef.eddy@gmail.com>
2018-07-30 12:42:50 -0500

    Javascript hangman werks yo

--- adnan_yousef/script.js
+++ adnan_yousef/script.js
@@ -1,10 +1,10 @@
-// var validInput = /^[a-zA-Z]$/.test(userGuess);
 var words = ['array','variable','object','boolean','string','function','method'];
 var wins = 0;
 var losses = 0;
 var guessesRemaining = 9;
 var partialWord = [];
 var guessesAlready = [];
+var guessesAlreadyWrong = [];
 
 // Generate random word
 var correctWord = words[Math.floor(Math.random() * words.length)];
@@ -15,12 +15,14 @@ for (i=0; i < correctWord.length; i++) {
     partialWord[i] = '_';
 }
 
+$("#wrapper").hide();
+
 function updateHTML() {
-    $("#wins").text(wins);
-    $("#losses").text(losses);
+    $("#wins").text("Wins: " + wins);
+    $("#losses").text("Losses: " + losses);
     $("#blanks").text(partialWord.join(''));
     $("#guesses-remaining").text(guessesRemaining);
-    $("#letters-already-guessed").text(guessesAlready);
+    $("#letters-already-guessed").text(guessesAlreadyWrong);
 }
 
 function updatePartialWord(index, letter) {
@@ -32,14 +34,17 @@ function reset(outcome) {
     if (outcome == 'win') {
         wins++;
         console.log("You win!");
+        $("#message").html("<h2 style=\"color: green;\">You won!")
     } else if (outcome == 'lose') {
         losses++;
         console.log("You lost...");
+        $("#message").html("<h2 style=\"color: red;\">You lost!")
     }
 
     guessesRemaining = 9;
     partialWord = [];
     guessesAlready = [];
+    guessesAlreadyWrong = [];
 
     // New random word
     correctWord = words[Math.floor(Math.random() * words.length)];
@@ -51,17 +56,14 @@ function reset(outcome) {
     updateHTML();
 };
 
-
-// Test if user guess is in word
-function test(letter) {
-    for (var z=0; z < correctWord.length; z++) {
-        if (correctWord.includes(letter)) {
-            updatePartialWord(z, letter)
-        };
-    };
-    updateHTML();
+// Test function, checks if you won or lost
+function testOutcome() {
+    if (!partialWord.includes('_')) {
+        reset('win');
+    } else if (guessesRemaining == 0) {
+        reset('lose');
+    }
 }
-
 
 
 
@@ -70,16 +72,37 @@ $(document).ready(function() {
     // On key press...
     $("html").keydown(function(event) {
 
+        // Hide "press to start"
+        $("#start").hide();
+        $("#wrapper").show();
+        $("#message").html('');
+
         var userGuess = event.key;
         console.log("Clicked: " + userGuess);
 
-        // Test if key is a character in correct word
-        for (var z=0; z < correctWord.length; z++) {
-            if (userGuess == correctWord[z]) {
-                updatePartialWord(z, userGuess)
+        // Check if you repeated a guess
+        if (guessesAlready.includes(userGuess)) {
+            alert('You already guessed ' + userGuess + '. Guess again!')
+            return;
+        }
+        guessesAlready.push(userGuess);
+
+        if (correctWord.includes(userGuess)) {
+            // Test if key is a character in correct word
+            for (var z=0; z < correctWord.length; z++) {
+                if (userGuess == correctWord[z]) {
+                    updatePartialWord(z, userGuess)
+                };
             };
+        } else {
+            // Decrement guesses remaining if not correct
+            guessesAlreadyWrong.push(userGuess);
+            guessesRemaining--;
+            console.log("Guesses remaining: " + guessesRemaining);
         };
 
+        // Run test to see if you are out of guesses or if you won
+        testOutcome();
 
         updateHTML();
     });
